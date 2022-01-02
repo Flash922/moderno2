76d89dd6140f88d893f0cb4ac78965197babd6f1
Flash922 <Flash92@gmail.com>
2022-01-02 21:31:07 +0300

    added new items page

--- app/js/main.js
+++ app/js/main.js
@@ -1,19 +1,39 @@
 $(function(){
-
   $('.rate-star').rateYo({
     rating: 5,
     starWidth: '12px',
     ratedFill: '#ffa726',
-    readOnly: true,
+    // readOnly: true,
   });
-
-  var mixer = mixitup('.products__inner-box');
-
+ 
   $('.product-slider__inner').slick({
     dots: true,
     arrows: false,
     slidesToShow: 4,
     slidesToScroll: 4,
+  }); 
+  
+  $('.js-range-slider').ionRangeSlider({
+    type: 'double',
+    skin: 'round',
+    grid: false,
+    hide_min_max: true,
+    min: 0,
+    max: 1200,
+    from:0,
+    to: 600,
+    prefix: '$',
   });
 
+  $('.icon-th-list').on('click', function(){
+    $('.product__item').addClass('list');
+    $('.icon-th-list').addClass('active');
+    $('.icon-th-large').removeClass('active');
+  });
+  $('.icon-th-large').on('click', function () {
+    $('.product__item').removeClass('list');
+    $('.icon-th-list').removeClass('active');
+    $('.icon-th-large').addClass('active');
+  });
+  var mixer = mixitup('.products__inner-box');
 });
